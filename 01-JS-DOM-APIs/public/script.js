8a76b6f8188b4c9a5af02a44535d6029eb009ee3
Lisandro Cooper <lisandro@co0per-K53E.home>
2018-02-23 19:10:39 -0300

    Adding some events

--- 01-JS-DOM-APIs/public/script.js
+++ 01-JS-DOM-APIs/public/script.js
@@ -2,3 +2,7 @@ function showSection() {
     document.getElementById("hide").style.visibility = "visible";
     document.getElementById("hide").style.opacity = "1";
 }
+
+function alertbox() {
+	alert("Hello! I am an alert box!");
+}
